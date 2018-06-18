3e4a94eba528662d3bdc4b202ccecba4a8128761
fuzileiros <40185429+fuzileiros@users.noreply.github.com>
2018-06-18 20:58:21 -0300

    Update socks5menu.sh

--- socks5menu.sh
+++ socks5menu.sh
@@ -1,12 +1,81 @@
  #!/bin/bash
 
 checkdatabase
+ip=$(wget -qO- ipv4.icanhazip.com)
+if [ $(id -u) -eq 0 ]
+then
+clear
+else
+echo -e "Execute o script como usuario \033[1;32mroot\033[0m"
+exit
+fi
+if [ -d /etc/VpsPackdir ]
+then
+true
+else
+mkdir /etc/VpsPackdir
+fi
+if [ -d /etc/VpsPackdir/senha ]
+then
+true
+else
+mkdir /etc/VpsPackdir/senha
+fi
+if [ -d /etc/VpsPackdir/limite ]
+then
+true
+else
+mkdir /etc/VpsPackdir/limite
+fi
 
 clear
-echo -e "\033[01;35m -------------------------------------------------"
-echo -e "\033[01;35m|                  \033[01;33m    ADM VPS\033[01;35m                   |"
-echo -e "\033[01;35m -------------------------------------------------"
-echo ""
+if [[ $1 == "" ]]
+then
+clear
+dia=$(date | awk {'print $3'})
+mes=$(date | awk {'print $2'})
+ano=$(date | awk {'print $6'})
+if [[ "$mes" = "Jan" || "$mes" = "Jan" ]]; then
+mes=01
+elif [[ "$mes" = "Feb" || "$mes" = "Fev" ]]; then
+mes=02
+elif [[ "$mes" = "Mar" || "$mes" = "Mar" ]]; then
+mes=03
+elif [[ "$mes" = "Apr" || "$mes" = "Abr" ]]; then
+mes=04
+elif [[ "$mes" = "May" || "$mes" = "Mai" ]]; then
+mes=05
+elif [[ "$mes" = "Jun" || "$mes" = "Jun" ]]; then
+mes=06
+elif [[ "$mes" = "Jul" || "$mes" = "Jul" ]]; then
+mes=07
+elif [[ "$mes" = "Ago" || "$mes" = "Ago" ]]; then
+mes=08
+elif [[ "$mes" = "Set" || "$mes" = "Sep" ]]; then
+mes=09
+elif [[ "$mes" = "Out" || "$mes" = "Oct" ]]; then
+mes=10
+elif [[ "$mes" = "Nov" || "$mes" = "Nov" ]]; then
+mes=11
+elif [[ "$mes" = "Dec" || "$mes" = "Dez" ]]; then
+mes=12
+fi
+echo -e "\033[7;30;46m ==========================================================\033[0m"
+echo -e "\033[7;30;47m  DATA:\033[0m\033[7;30;41m $dia/$mes/$ano\033[0m"
+echo -e "\033[7;30;47m  IP da VPS:\033[0m\033[7;30;41m $ip\033[0m"
+system=$(cat /etc/issue.net)
+if [ "$system" ]
+then
+echo -e "\033[7;30;47m  Sistema Operacional:\033[0m\033[7;30;41m $system\033[0m"
+else
+echo -e "\033[7;30;47m  Sistema Operacional:\033[0m\033[7;30;41mNot Available\033[0m"
+fi
+echo -e "\033[7;30;46m ==========================================================\033[0m"
+echo -e "\033[7;30;47m  Â®Script\033[0m \033[7;30;41mEdited:\033[0m \033[0m \033[0;36;40m@Colt\033[5;37;40mSeals\033[0m"
+echo -e "\033[7;30;47m  Â®TELEGRAM\033[0m \033[7;30;41mCANAL:\033[0m \033[0m \033[0;36;40m@Vps\033[5;37;40mNerdologia\033[0m"
+echo -e "\033[7;30;46m ==========================================================\033[0m"
+echo -e "\033[7;30;46m  Para Acessar este menu digite:\033[5;37;40m VPN \033[0m"
+echo -e "\033[7;30;46m ==========================================================\033[0m"
 NUMBER=$(ps -x | grep -c "socks5.py")
 if [ $NUMBER = "2" ]; then
   PORT=$(cat /home/DATABASE/SOCKS5/socks5.py | sed -n '300p' | cut -d "=" -f2 | cut -d " " -f2)
@@ -19,7 +88,7 @@ if [ $NUMBER = "2" ]; then
   echo -e "\033[01;32m  3 \033[01;35m-\033[01;31m DESATIVAR\033[01;33m SOCKS5."
   echo -e "\033[01;32m  0 \033[01;35m-\033[01;33m MENU INICIAL"
   echo -e "\033[01;35m -------------------------------------------------"
-  echo -ne "\033[1;33m  OPÇÃO \033[01;32m[1-3]\033[1;37m:"; read NUMBERS
+  echo -ne "\033[1;33m  OPÃ‡ÃƒO \033[01;32m[1-3]\033[1;37m:"; read NUMBERS
   case $NUMBERS in
     1) socks5banner;;
     2) socks5changeport;;
@@ -29,7 +98,7 @@ if [ $NUMBER = "2" ]; then
           sleep 3s
           socks5menu
           exit;;
-    0) h;;
+    0) vpn;;
       *) socks5menu;;
   esac
 else
@@ -43,7 +112,7 @@ else
   echo -e "\033[01;32m  3 \033[01;35m-\033[01;32m ATIVAR\033[01;33m SOCKS5."
   echo -e "\033[01;32m  0 \033[01;35m-\033[01;33m MENU INICIAL"
   echo -e "\033[01;35m -------------------------------------------------"
-  echo -ne "\033[1;33m  OPÇÃO \033[01;32m[1-3]\033[1;37m:"; read NUMBERS
+  echo -ne "\033[1;33m  OPÃ‡ÃƒO \033[01;32m[1-3]\033[1;37m:"; read NUMBERS
   case $NUMBERS in
     1) socks5banner;;
     2) socks5changeport;;
@@ -53,7 +122,7 @@ else
           sleep 3s
           socks5menu
           exit;;
-    0) h;;
+    0) vpn;;
       *) socks5menu;;
   esac
 fi
