eed9ca559eda861b3e9ce310d65bceb1ae9b72b1
fuzileiros <40185429+fuzileiros@users.noreply.github.com>
2018-06-26 08:39:16 -0300

    Update and rename prox.py to pro.py

--- pro.py
+++ pro.py
@@ -2,8 +2,9 @@ import sys, time, getopt, socket, threading, base64
 
 
 # CONFIG
-CONFIG_LISTENING = input ('Digite o ip:porta')
-CONFIG_PASS = input ('Digite sua senha')
+CONFIG_LISTENING = str(input ('Digite o ip:porta'))
+
+CONFIG_PASS = str(input ('Digite sua senha'))
 
 
 class Logger:
